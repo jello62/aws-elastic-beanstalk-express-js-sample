bf1914072b076452fbdb10ad0916ec3d79f75f6f
jello62 <56134062+jello62@users.noreply.github.com>
2021-02-05 20:47:10 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Greeting from Jen!'));
+app.get('/', (req, res) => res.send('Greeting from Jen! Added approval stage.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
